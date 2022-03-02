49fe85f26019178fbc5429b0a9ca673a2dde56eb
MuktadirHassan <tamimmuktadir@gmail.com>
2022-03-02 23:06:58 +0600

    sample env added

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,58 +1,61 @@
-const  {createResponse}   =   require('../utils/responseGenerate');
-const cloudinary   		  =   require('../lib/cloudinary');
-const Product   		  =   require('../models/Product');
-
+const { createResponse } = require("../utils/responseGenerate");
+const cloudinary = require("../lib/cloudinary");
+const Product = require("../models/Product");
 
 module.exports.createProduct = async (req, res, next) => {
-	try{
-		const body = req.body;
-		const upload = await cloudinary.uploader.upload(req.file.path);
+  try {
+    const body = req.body;
+    const upload = await cloudinary.uploader.upload(req.file.path);
 
-		body.image = upload.public_id;
-		const product = new Product(body);
-		await product.save();
-		return res.status(201).json(createResponse(product, 'Product Added successfully!', false));
-	} catch(err) {
-		next(err);
-	}
+    body.image = upload.public_id;
+    const product = new Product(body);
+    await product.save();
+    return res
+      .status(201)
+      .json(createResponse(product, "Product Added successfully!", false));
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports.deleteProduct = async (req, res, next) => {
-	try{
-		const { id } = req.params;
-		const product = await Product.deleteOne({_id: id});
-		if(product.deletedCount) {
-			return res.status(200).json(createResponse(null, 'Product Deleted successfully!', false));
-		}
-		return res.status(404).json(createResponse(null, 'No Product found with this Id!', true));
-	} catch(err) {
-		next(err);
-	}
+  try {
+    const { id } = req.params;
+    const product = await Product.deleteOne({ _id: id });
+    if (product.deletedCount) {
+      return res
+        .status(200)
+        .json(createResponse(null, "Product Deleted successfully!", false));
+    }
+    return res
+      .status(404)
+      .json(createResponse(null, "No Product found with this Id!", true));
+  } catch (err) {
+    next(err);
+  }
 };
 /**
  * Get Product By Id
  * @function
  * @param {req}
- */ 
+ */
 module.exports.getProductById = async (req, res, next) => {
-	try{
-		const { id } = req.params;
-		const product = await Product.findOne({_id: id});
-		if(!product) throw new Error('No product found with this id!');
-		return res.json(createResponse(product));
-	} catch(err){
-		next(err);
-	}
+  try {
+    const { id } = req.params;
+    const product = await Product.findOne({ _id: id });
+    if (!product) throw new Error("No product found with this id!");
+    return res.json(createResponse(product));
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports.getProducts = async (req, res, next) => {
-	try{
-		const query = req.query;
-		const products = await Product.find(query);
-		return res.json(createResponse(products));
-	} catch(err){
-		next(err);
-	}
+  try {
+    const query = req.query;
+    const products = await Product.find(query);
+    return res.json(createResponse(products));
+  } catch (err) {
+    next(err);
+  }
 };
-
-
--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-const express   =   require('express');
-const app       =   express();
-const routes    =   require('./routes/index');
-const db        =   require('./db/db');
-const errorHandler  =   require('./middlewares/errors');
-const passport = require('passport');
-const cors     = require('cors');
+const express = require("express");
+const app = express();
+const routes = require("./routes/index");
+const db = require("./db/db");
+const errorHandler = require("./middlewares/errors");
+const passport = require("passport");
+const cors = require("cors");
 
-require('dotenv').config({path: `${__dirname}/../.env`});
+require("dotenv").config({ path: `${__dirname}/../.env` });
 // Pass the global passport object into the configuration function
 app.use(cors());
 
-require('./lib/passport')(passport);
+require("./lib/passport")(passport);
 app.use(passport.initialize());
 app.use(express.json());
-app.use('/api', routes);
-
+app.use("/api", routes);
 
 //error handler
 app.use(errorHandler);
-const port      =   process.env.PORT || 3000;
-app.listen(port, () => console.log(`App is listening at http://localhost:${port}`));
+const port = process.env.PORT || 3000;
+app.listen(port, () =>
+  console.log(`App is listening at http://localhost:${port}`)
+);
 
 module.exports = app;
